35241ccb4daca8a90185e5167bd0e7daa2d5361c
Bourama FANE <bouramayaya@gmail.com>
2023-11-27 10:31:33 +0000

    Dashboard

--- concat_csv_files.py
+++ concat_csv_files.py
@@ -1,32 +1,22 @@
-import os
 import pandas as pd
+import requests
+from io import StringIO
 
-def concat_csv_files(folder_path, keyword):
-    # Obtenez la liste des fichiers dans le dossier
-    files = [file for file in os.listdir(folder_path) if file.endswith('.csv') and keyword in file]
+# URL brute du fichier CSV sur GitHub
+github_csv_url = 'https://github.com/bouramayaya/dashboard/tree/master/data/X_train_1.csv'
 
-    # Vérifiez si des fichiers correspondants ont été trouvés
-    if not files:
-        print(f"Aucun fichier correspondant au mot-clé '{keyword}' trouvé.")
-        return None
+# Utilisez requests pour obtenir le contenu du fichier CSV
+response = requests.get(github_csv_url)
 
-    # Initialisez une liste pour stocker les DataFrames
-    dfs = []
+# Vérifiez si la requête a réussi (statut 200)
+if response.status_code == 200:
+    # Utilisez StringIO pour lire le contenu comme un fichier
+    csv_content = StringIO(response.text)
+    
+    # Utilisez pandas pour lire le CSV depuis le contenu
+    df = pd.read_csv(csv_content)
 
-    # Parcourez chaque fichier, lisez-le et ajoutez le DataFrame à la liste
-    for file in files:
-        file_path = os.path.join(folder_path, file)
-        df = pd.read_csv(file_path)
-        dfs.append(df)
-
-    # Concaténez tous les DataFrames dans la liste en un seul DataFrame
-    concatenated_df = pd.concat(dfs, ignore_index=True)
-
-    return concatenated_df
-
-
-folder_path = '/home/ubuntu/OC/OC-Projet-7/data'
-# folder_path = 'C:/Users/Fane0763/OpenClassroom/OC Projet 7/OC-Projet-7/data'
-keyword = 'X_train_'
-result_df = concat_csv_files(folder_path, keyword)
-print(result_df.shape)
+    # Affichez les premières lignes du DataFrame
+    print(df.head())
+else:
+    print(f"Échec de la requête avec le code d'état {response.status_code}")
